22b3ab9919c505a33480797b079485369087178c
Kanchana Ranasinghe <kahnchana@gmail.com>
2021-06-07 16:09:30 +0400

    update pretrained models

--- scripts/visualize_segmentation.sh
+++ scripts/visualize_segmentation.sh
@@ -3,7 +3,7 @@
 # SIN distilled model (shape token)
 python evaluate_segmentation.py \
   --model_name "dino_small_dist" \
-  --pretrained_weights "ckpts/SIN/DeiT_S_SIN_dist/checkpoint.pth" \
+  --pretrained_weights "https://github.com/Muzammal-Naseer/Intriguing-Properties-of-Vision-Transformers/releases/download/v0/deit_s_sin_dist.pth" \
   --threshold 0.9 \
   --patch_size 16 \
   --use_shape \
